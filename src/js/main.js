0fbe8194bed08aa90af9fef0708901f2574c31e4
SineYlo <vavshhik01@mail.ru>
2022-02-23 18:30:31 +1000

    info: the template is set up and ready to write code

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,42 @@
+/* eslint-disable no-unused-vars */
 // |=============== CONNECTING EXTERNAL MODULES ===============>
-// import module_name from 'module-name';
+import focusVisible from 'focus-visible';
 
-// |=============== SCRIPT CONNECTION |  ===============>
-// |=============== SCRIPT CONNECTION |  ===============>
-// |=============== SCRIPT CONNECTION |  ===============>
-// |=============== SCRIPT CONNECTION |  ===============>
-// |=============== SCRIPT CONNECTION |  ===============>
+// |=============== SCRIPT CONNECTION | ACCORDION  ===============>
+import accordion from './components/home/accordion';
+// |=============== SCRIPT CONNECTION | ANCHOR VISIBLE  ===============>
+import anchorVisible from './components/home/anchor-visible';
+// |=============== SCRIPT CONNECTION | BURGER  ===============>
+import burger from './components/home/burger';
+// |=============== SCRIPT CONNECTION | DROPDOWN  ===============>
+import dropdown from './components/home/dropdown';
+// |=============== SCRIPT CONNECTION | FORM MODAL  ===============>
+import formModal from './components/home/form-modal';
+// |=============== SCRIPT CONNECTION | GALLERY MODAL  ===============>
+import galleryModal from './components/home/gallery-modal';
+// |=============== SCRIPT CONNECTION | LOADER  ===============>
+import loader from './components/home/loader';
+// |=============== SCRIPT CONNECTION | YANDEX MAP  ===============>
+import map from './components/home/map';
+// |=============== SCRIPT CONNECTION | PICTURE ===============>
+import picture from './components/home/picture';
+// |=============== SCRIPT CONNECTION | SUPER PLACEHOLDER  ===============>
+import placeholder from './components/home/placeholder';
+// |=============== SCRIPT CONNECTION | SIMPLEBAR  ===============>
+import scrollCustom from './components/home/scroll-custom';
+// |=============== SCRIPT CONNECTION | SCROLL INDENT  ===============>
+import scrollIndent from './components/home/scroll-indent';
+// |=============== SCRIPT CONNECTION | SCROLL SMOOTH  ===============>
+import scrollSmooth from './components/home/scroll-smooth';
+// |=============== SCRIPT CONNECTION | SEARCH  ===============>
+import search from './components/home/search';
+// |=============== SCRIPT CONNECTION | CHOICES  ===============>
+import select from './components/home/select';
+// |=============== SCRIPT CONNECTION | SWIPER  ===============>
+import slider from './components/home/slider';
+// |=============== SCRIPT CONNECTION | TAB  ===============>
+import tab from './components/home/tab';
+// |=============== SCRIPT CONNECTION | TIPPY  ===============>
+import tooltip from './components/home/tooltip';
+// |=============== SCRIPT CONNECTION | JUST VALIDATE ===============>
+import validate from './components/home/validate';
